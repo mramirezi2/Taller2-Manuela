352ca60084bd5268ff74b680d60b96504805dd88
Manuela Ramírez <manuelaiturralde@gmail.com>
2026-01-28 12:34:09 -0500

    Serie de la columna con NaN

--- Taller1M.py
+++ Taller1M.py
@@ -6,3 +6,8 @@ data_frame.shape
 print(data_frame.head())
 
 data_frame.isna().sum(axis=0)
+
+indice = df["Brand"]
+valor = df["Ex_Showroom_Price"]
+
+ser = pd.Series(valor.values, index=indice.values)

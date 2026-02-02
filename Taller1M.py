4b9b0392b7b17af9ffaf0afa4a56e935c2f819b8
Paula Silva <paula.silva0507@hotmail.com>
2026-02-02 09:46:34 -0500

    Paula - agregar promedio del precio

--- Taller1M.py
+++ Taller1M.py
@@ -11,3 +11,9 @@ indice = df["Brand"]
 valor = df["Ex_Showroom_Price"]
 
 ser = pd.Series(valor.values, index=indice.values)
+
+#Modificaciones Paula
+promedios = df.groupby("Brand")["Ex_Showroom_Price"].mean() 
+df2 = df.copy() 
+rellenar = df["Brand"].map(promedios) 
+df2["Ex_Showroom_Price"] = df2["Ex_Showroom_Price"].fillna(rellenar) 
